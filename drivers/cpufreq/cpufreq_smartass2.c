a841c29de6b48b86e1e0ee7928d47dda5c53e86d
giugiu19 <gpirasking@outlook.it>
2018-02-19 15:05:40 +0100

    Cpufreq :Smartassv2 :Config PowerSuspend

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -897,6 +897,10 @@ static int __init cpufreq_smartass_init(void)
 #ifdef CONFIG_HAS_EARLYSUSPEND
 	register_early_suspend(&smartass_power_suspend);
 #endif
+	
+#ifdef CONFIG_POWERSUSPEND
+	register_power_suspend(&smartass_power_suspend);
+#endif
 
 	return cpufreq_register_governor(&cpufreq_gov_smartass2);
 }
